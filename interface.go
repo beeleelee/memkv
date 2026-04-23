2176ff4e972ddeff8e73006fc57fdf67f619031d
beeleelee <beeleelee@outlook.com>
2026-04-23 09:15:43 +0800

    define interface

---
+++ interface.go
@@ -0,0 +1,9 @@
+package memkv
+
+type KVDB interface {
+	Put(key, value []byte) error
+	Delete(key []byte) error
+	// Contains(key []byte) bool
+	Get(key []byte) (value []byte, err error)
+	// Find(key []byte) (rkey, value []byte, err error)
+}
