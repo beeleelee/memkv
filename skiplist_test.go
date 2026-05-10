906f18eeb8bfb64104183a7d5c1a3bd2c3293eb0
beeleelee <beeleelee@outlook.com>
2026-05-10 10:29:20 +0800

    test/benckmark testing

--- skiplist_test.go
+++ skiplist_test.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math/rand"
 	"testing"
+	"time"
 )
 
 func TestContainsFind(t *testing.T) {
@@ -39,22 +40,11 @@ func TestContainsFind(t *testing.T) {
 	}
 }
 func TestPutGetDelete(t *testing.T) {
-	var keys []string
-	vals := []string{"golang", "leveldb", "postgres", "redis", "kubernets", "git", "docker", "ollama", "rag", "nginx", "mysql", "java", "rocm", "ubuntu", "npm", "ansible", "bitcoin", "xfs", "awk", "sed", "curl", "tmux", "tr", "tee", "df", "apt", "systemctl", "ssh", "less", "vim", "touch", "mv", "cd", "cp", "scp", "jump", "box", "kafka", "ln", "cat", "tar", "which", "alias", "date", "echo", "cut", "ping", "uptime", "ps", "top"}
-	var values []string
-	for i := 0; i < 10; i++ {
-		values = append(values, vals...)
-	}
-	keys = make([]string, len(values))
-	for i := range values {
-		keys[i] = fmt.Sprintf("%03d", i)
-	}
+	keys, values := genTestKeyValue()
 	l := len(keys)
-	rand.Shuffle(l, func(i, j int) {
-		keys[i], keys[j] = keys[j], keys[i]
-	})
+	var db KVDB
 	sl := newSkipList()
-	db := sl
+	db = sl
 	// put empty value first
 	for i := 0; i < l; i++ {
 		if err := db.Put([]byte(keys[i]), nil); err != nil {
@@ -103,4 +93,60 @@ func TestPutGet(t *testing.T) {
 	} else if !bytes.Equal(v, rv) {
 		log.Fatal("value not match", v, rv)
 	}
+}
+
+func BenchmarkPutGet(t *testing.B) {
+	st1 := time.Now()
+	db := New()
+	keys, values := genBenchmarkKeyValue(t.N)
+	st2 := time.Now()
+	dur1 := st2.Sub(st1)
+	fmt.Printf("key value prepared elapsed: %d, op %d, t.N %d\n", dur1.Nanoseconds(), dur1.Nanoseconds()/int64(t.N), t.N)
+	for i := 0; i < t.N; i++ {
+		if err := db.Put(keys[i], values[i]); err != nil {
+			t.Fatal(err)
+		}
+	}
+	st3 := time.Now()
+	dur2 := st3.Sub(st2)
+	fmt.Printf("put finished elaspsed: %d, op %d, t.N %d\n", dur2.Nanoseconds(), dur2.Nanoseconds()/int64(t.N), t.N)
+	for i := 0; i < t.N; i++ {
+		v, err := db.Get(keys[i])
+		if err != nil {
+			t.Fatal(err)
+		} else if !bytes.Equal(v, values[i]) {
+			t.Fatal("key ", string(keys[i]), " not match, expect ", string(values[i]), " got ", string(v))
+		}
+	}
+	st4 := time.Now()
+	dur3 := st4.Sub(st3)
+	fmt.Printf("get finished elaspsed: %d, op %d, t.N %d\n", dur3.Nanoseconds(), dur3.Nanoseconds()/int64(t.N), t.N)
+}
+
+func genBenchmarkKeyValue(n int) ([][]byte, [][]byte) {
+	keys := make([][]byte, n)
+	values := make([][]byte, n)
+	for i := 0; i < n; i++ {
+		keys[i] = []byte(fmt.Sprintf("key-%d", i))
+		values[i] = []byte(fmt.Sprintf("value-%d", i))
+	}
+	return keys, values
+}
+
+func genTestKeyValue() ([]string, []string) {
+	var keys []string
+	vals := []string{"golang", "leveldb", "postgres", "redis", "kubernets", "git", "docker", "ollama", "rag", "nginx", "mysql", "java", "rocm", "ubuntu", "npm", "ansible", "bitcoin", "xfs", "awk", "sed", "curl", "tmux", "tr", "tee", "df", "apt", "systemctl", "ssh", "less", "vim", "touch", "mv", "cd", "cp", "scp", "jump", "box", "kafka", "ln", "cat", "tar", "which", "alias", "date", "echo", "cut", "ping", "uptime", "ps", "top"}
+	var values []string
+	for i := 0; i < 10; i++ {
+		values = append(values, vals...)
+	}
+	keys = make([]string, len(values))
+	for i := range values {
+		keys[i] = fmt.Sprintf("%03d", i)
+	}
+	l := len(keys)
+	rand.Shuffle(l, func(i, j int) {
+		keys[i], keys[j] = keys[j], keys[i]
+	})
+	return keys, values
 }
